11285d43e890540071dffb3dcfb363a46ce88de1
reifred <reifred33@gmail.com>
2019-11-04 15:19:06 +0300

    Update links and modify styles

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
-const deploy = require("gulp-gh-pages")
 
 
 // BrowserSync
